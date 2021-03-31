0636dc2120148685a4679f7390b86ae3c5102305
ezeswci <wernicke.ezequiel@gmail.com>
2021-03-31 17:18:36 +1100

    Add a sample endpoint

--- workers/loc.api/sample.js
+++ workers/loc.api/sample.js
@@ -1,7 +1,15 @@
 const { Api } = require('bfx-wrk-api')
 
 class Sample extends Api {
-
+  async sampleEndpoint (space, token, cb) {
+    try {
+      const authGoolge = this.ctx.auth_google
+      const res = await authGoolge.preAdminTokenCheck(token)
+      cb(null, res)
+    } catch (e) {
+      cb(e)
+    }
+  }
 }
 
 module.exports = Sample
